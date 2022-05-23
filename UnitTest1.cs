b34e4ebeca5444f0b7aaf051f38e12e010a0dea6
alexeiastrakh <92253303+alexeiastrakh@users.noreply.github.com>
2022-05-23 17:23:42 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpaceWebApplication.Models;
+using SpaceWebApplication.Controllers;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async void Test1()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DBMissionContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-DMI7F36; Database=DBMissionAPI; Trusted_Connection=True; MultipleActiveResultSets=true");
+
+            var controller = new CitiesController(new DBMissionContext(optionsBuilder.Options));
+
+            // Act
+            var result = await controller.GetCity();
+
+            // Assert
+            Assert.Contains(result.Value, c => c.Name.Equals("Paris"));
+
+        }
+    }
+}
---
+++ UnitTest2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceWebApplication.Models;
+using SpaceWebApplication.Controllers;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public async void Test2()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DBMissionContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-DMI7F36; Database=DBMissionAPI; Trusted_Connection=True; MultipleActiveResultSets=true");
+
+            var controller = new CitiesController(new DBMissionContext(optionsBuilder.Options));
+            
+            // Act
+            var result = await controller.GetCity();
+
+            // Assert
+            Assert.Contains(result.Value, c => c.Name.Equals("Mexico"));
+
+        }
+    }
+}
